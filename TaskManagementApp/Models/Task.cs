133b4c2b99678616a391b7b1a9b22786d10ea7da
Finnley Grange <dev.finnleygrange@icloud.com>
2024-03-06 23:59:31 +0000

    Add project files.

---
+++ TaskManagementApp/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace TaskManagementApp.Models
+{
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High,
+    }
+
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [DisplayName("Due Date")]
+        public DateTime DueDate { get; set; }
+        public Priority Priority { get; set; }
+        [DisplayName("Is Completed")]
+        public bool IsCompleted { get; set; }
+    }
+}
